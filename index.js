d42a261113e31ac94773727a6a2cf95a0dd294ea
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-08-17 21:09:25 -0400

    commit tree 063679390803907a54e2b2d02287942945beb82e

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = (activity = "go to the office") => {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(outerParam = "*") {
+  return (innerParam = "special") => {
+    return `You are ${outerParam}${innerParam}${outerParam}!`
+  }
+}
+
+const Calculator = {
+  add: (a, b) => {
+    return a + b;
+  },
+  subtract: (a, b) => {
+    return a - b;
+  },
+  multiply: (a, b) => {
+    return a * b;
+  },
+  divide: (a, b) => {
+    return a / b;
+  }
+}
+
+function actionApplyer(startInt, funcArr) {
+  let answer = startInt;
+  if (funcArr.length > 0) {
+    for (const element of funcArr) {
+      answer = element(answer)
+    };
+  }
+  return answer;
+}
